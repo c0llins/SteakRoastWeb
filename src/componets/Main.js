19ede1870c41dd4b84f89fe688d5915113890218
c0llins <collins.dev@icloud.com>
2019-09-07 11:22:27 -0300

    :tada: Initial commit

---
+++ src/componets/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../css/App.css";
+import Navbar from "./Navbar";
+function Main() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+    </div>
+  );
+}
+
+export default Main;
---
+++ src/componets/Navbar.js
@@ -0,0 +1,36 @@
+import React from "react";
+// import "../css/App.css";
+function Navbar() {
+  return (
+    <nav
+      class="navbar is-fixed-top"
+      role="navigation"
+      style="flex-grow: 1; justify-content: center;"
+      aria-label="main navigation"
+    >
+      <div class="navbar-brand">
+        <a class="navbar-item" href="https://bulma.io">
+          <img
+            src="https://bulma.io/images/bulma-logo.png"
+            alt="Bulma: Free, open source, & modern CSS framework based on Flexbox"
+            width="112"
+            height="28"
+          />
+        </a>
+
+        <a
+          role="button"
+          class="navbar-burger"
+          aria-label="menu"
+          aria-expanded="false"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
