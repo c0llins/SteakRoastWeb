4dc175fb4287056b986118ca7e4d547d3f0aa543
c0llins <collins.dev@icloud.com>
2019-09-08 13:02:46 -0300

    :bento: Create Card.js

---
+++ src/componets/card/Card.js
@@ -0,0 +1,13 @@
+import React from "react";
+import CardImage from "./CardImage";
+import CardContent from "./CardContent";
+
+function Card() {
+  return (
+    <div class="card">
+      <CardImage></CardImage>
+      <CardContent></CardContent>
+    </div>
+  );
+}
+export default Card;
