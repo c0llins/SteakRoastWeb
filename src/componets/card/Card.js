696916eefd864ad40d686ac066b0f4431b576841
c0llins <collins.dev@icloud.com>
2019-09-08 18:04:21 -0300

    :alien: Update Card

--- src/componets/card/Card.js
+++ src/componets/card/Card.js
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
+import CardTitle from "./CardTitle";
 import CardImage from "./CardImage";
-import CardContent from "./CardContent";
+import CardTag from "./CardTag";
+import CardPrice from "./CardPrice";
 
-function Card() {
-  return (
-    <div class="card">
-      <CardImage></CardImage>
-      <CardContent></CardContent>
-    </div>
-  );
+class Card extends Component {
+  render() {
+    const tags = this.props.tags.map(function(tags) {
+      return <CardTag name={tags}></CardTag>;
+    });
+
+    return (
+      <div class="card">
+        <CardImage
+          imgLink={this.props.imgLink}
+          imgAlt={this.props.imgAlt}
+        ></CardImage>
+        <div class="card-content">
+          <CardTitle name={this.props.name}></CardTitle>
+          <div class="content">
+            <div class="tags are-medium">{tags}</div>
+            <CardPrice price={12.2}></CardPrice>
+            <a
+              class="button is-medium is-fullwidth is-success"
+              href={this.props.whatsLink}
+            >
+              <i class="fab fa-whatsapp" style={{ marginRight: "5px" }}></i>
+              Pedir
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
+
 export default Card;
--- src/componets/card/CardImage.js
+++ src/componets/card/CardImage.js
@@ -1,13 +1,10 @@
 import React from "react";
 
-function CardImage() {
+function CardImage(props) {
   return (
     <div class="card-image">
       <figure class="image is-4by3">
-        <img
-          src="https://yata.ostr.locaweb.com.br/9709eaea2a20f9ad958d6bb3d6c1ffeae7b02eaa1af5e7bfefc4df8de35d47f4"
-          alt="Placeholder image"
-        />
+        <img src={props.imgLink} alt={props.imgAlt} />
       </figure>
     </div>
   );
