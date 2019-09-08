cffa2f54e739ba4fe6b73f1d7fa62508ddbdd5d5
c0llins <collins.dev@icloud.com>
2019-09-08 13:03:12 -0300

    :bento: Create CardImage.js

---
+++ src/componets/card/CardImage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function CardImage() {
+  return (
+    <div class="card-image">
+      <figure class="image is-4by3">
+        <img
+          src="https://yata.ostr.locaweb.com.br/9709eaea2a20f9ad958d6bb3d6c1ffeae7b02eaa1af5e7bfefc4df8de35d47f4"
+          alt="Placeholder image"
+        />
+      </figure>
+    </div>
+  );
+}
+export default CardImage;
