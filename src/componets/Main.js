8c30a8900c026e199d24aa3f5cc024d5a3f85843
c0llins <collins.dev@icloud.com>
2019-09-08 13:06:23 -0300

    :ok_hand: Update Main.js

--- src/componets/Main.js
+++ src/componets/Main.js
@@ -1,10 +1,58 @@
 import React from "react";
 import "../css/App.css";
 import Navbar from "./Navbar";
+import Card from "./card/Card";
+
 function Main() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <section class="section" style={{ marginTop: "10px" }}>
+        <div class="container">
+          <div class="columns">
+            <div class="column">
+              <Card></Card>
+            </div>
+            <div class="column">
+              <Card></Card>
+            </div>
+            <div class="column">
+              <Card></Card>
+            </div>
+            <div class="column">
+              <Card></Card>
+            </div>
+          </div>
+          <div class="columns">
+            <div class="column">
+              <Card></Card>
+            </div>
+            <div class="column">
+              <Card></Card>
+            </div>
+            <div class="column">
+              <Card></Card>
+            </div>
+            <div class="column">
+              <Card></Card>
+            </div>
+          </div>
+          <div class="columns">
+            <div class="column">
+              <Card></Card>
+            </div>
+            <div class="column">
+              <Card></Card>
+            </div>
+            <div class="column">
+              <Card></Card>
+            </div>
+            <div class="column">
+              <Card></Card>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
