e2680933cce6df776894527d1218aa452647841d
c0llins <collins.dev@icloud.com>
2019-09-09 01:26:25 -0300

    :alien: Updating code due to external API changes.

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,37 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Card from "../../componets/card/Card";
 import Navbar from "../../componets/Navbar";
+import api from "../../services/api";
 
 export default function Index() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    api.get("/view").then(result => setData(result.data));
+  }, []);
+
   return (
     <div className="home">
       <Navbar></Navbar>
-      <section class="section" style={{ marginTop: "10px" }}>
-        <div class="container">
-          <div class="columns">
-            <div class="column">
-              <Card
-                name={"Costelinha Bovina"}
-                price={12.9}
-                tags={["Carne", "Tomate"]}
-                whatsLink={
-                  "https://api.whatsapp.com/send?phone=5547988909401&text=*NOME%20LANCHE*%0D%0DCom%20%20_%20-%20Alface%20%2C%20Calabresa%2C%20Cheddar%2C%20Hambúrguer%2C%20Tomate%20_"
-                }
-                imgLink={
-                  "https://yata.ostr.locaweb.com.br/9709eaea2a20f9ad958d6bb3d6c1ffeae7b02eaa1af5e7bfefc4df8de35d47f4"
-                }
-                imgAlt={"Costelinha Bovina"}
-              ></Card>
-            </div>
-            <div class="column">
-              <Card></Card>
-            </div>
-            <div class="column">
-              <Card></Card>
-            </div>
-            <div class="column">
-              <Card></Card>
-            </div>
+      <section className="section" style={{ marginTop: "10px" }}>
+        <div className="container">
+          <div
+            className="columns"
+            style={{ display: "flex", flexWrap: "wrap" }}
+          >
+            {data.map(item => (
+              <div className="column is-one-quarter">
+                <Card
+                  name={item.name}
+                  price={item.price}
+                  tags={item.ingredients}
+                  whatsLink={
+                    "https://api.whatsapp.com/send?phone=5547988909401&text=*NOME%20LANCHE*%0D%0DCom%20%20_%20-%20Alface%20%2C%20Calabresa%2C%20Cheddar%2C%20Hambúrguer%2C%20Tomate%20_"
+                  }
+                  imgLink={item.img}
+                  imgAlt={item.name}
+                ></Card>
+              </div>
+            ))}
           </div>
         </div>
       </section>
